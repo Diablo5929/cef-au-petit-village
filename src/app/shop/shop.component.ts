9ba951b0da223e5b4e854155f35b3d55ec5ee4f9
Diablo <hanon.mickael59@gmail.com>
2024-10-20 23:18:18 +0200

    first commit

---
+++ src/app/shop/shop.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { ShopDataService } from '../services/shop-data.service';
+import { TitreH3Component } from '../titres/titre-h3/titre-h3.component';
+import { FormsModule } from '@angular/forms';
+import { FiltrePipe } from '../pipes/filtre.pipe';
+import { SortPipe } from '../pipes/sort.pipe';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-shop',
+  standalone: true,
+  imports: [
+    NgFor,
+    TitreH3Component,
+    FormsModule,
+    FiltrePipe,
+    SortPipe,
+    RouterLink,
+  ],
+  templateUrl: './shop.component.html',
+  styleUrl: './shop.component.css',
+})
+export class ShopComponent implements OnInit {
+  searchTerm: string = '';
+  tri: string = '';
+
+  productList: any = {};
+  constructor(private shopDataService: ShopDataService) {}
+
+  ngOnInit(): void {
+    this.productList = this.shopDataService.productList;
+  }
+}
